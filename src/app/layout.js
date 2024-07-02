e9c9ce3b0c9aaeac5b96cf468e7ec6e9bc64f6de
Thomas Hallström <minimoto@hotmail.com>
2024-07-02 15:15:08 +0200

    with insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
