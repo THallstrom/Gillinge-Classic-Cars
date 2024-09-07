174a1e4cfa0ae6fdbc80241c264f7f271ee5accb
Thomas Hallström <minimoto@hotmail.com>
2024-09-07 21:11:37 +0200

    Change in img size in cars regarding mobil

--- src/app/cars/page.js
+++ src/app/cars/page.js
@@ -5,20 +5,22 @@ import Link from "next/link";
 function Page() {
     return (
         <main className="min-h-screen p-5">
-            <p>Våra bilar</p>
-            <div className="grid gap-5 pt-6 sm:flex">
+                <p className="text-center">Våra bilar</p>
+            <div className="grid gap-3 pt-6 justify-center w-full md:flex">
                 {carData.map((car, index) => (
-                    <div key={index}>
-                        <div className="min-h-72 max-h-72 w-56 rounded-md p-3 hover:shadow-xl">
+                    <div key={index} className="w-full md:w-auto">
+                        <div className="min-h-72 max-h-72 w-full md:w-56 rounded-md p-3 hover:shadow-xl">
                             <Link href={`/carDetail/${car.Id}`} passHref>
                                 <Image
                                     src={car.Images.MainImage}
                                     alt="Test bild"
                                     width={500}
                                     height={500}
-                                    className="h-56 w-56 object-cover rounded-md shadow-md"
+                                    className="w-full h-56 object-cover rounded-md shadow-md"
                                 />
-                                {car.Brand} {car.Model}
+                                <p className="text-center">
+                                    {car.Brand} {car.Model}
+                                </p>
                             </Link>
                         </div>
                     </div>
