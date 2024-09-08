a135d9caa3b74c8672e3840b0be322d7cc9202f3
MrMinken <minimoto@hotmail.com>
2024-09-08 14:44:44 +0200

    change in info or data text

--- src/app/carDetail/[Id]/page.js
+++ src/app/carDetail/[Id]/page.js
@@ -4,16 +4,25 @@ import { usePathname } from "next/navigation";
 import carData from "/public/json/carData.json";
 import Image from "next/image";
 import Button from "@/components/buttonDetail";
+import { useState } from "react";
+import BtnTest from "@/components/btnTest";
 
 function Page(test) {
     const car = carData.find((car) => car.Id == test.params.Id);
     if (!car) {
         return <p>Car not found</p>;
     }
+
+    const [selectedInfo, setSelectedInfo] = useState("Data");
+
+    const handleButtonClick = (option) => {
+        setSelectedInfo(option);
+    };
+
     return (
         <div className="p-3 flex flex-col w-full sm:flex-row">
             <div className="sm:w-1/2">
-            <h1 className="block text-center sm:hidden">
+                <h1 className="block text-center sm:hidden">
                     {car.Brand} {car.Model}
                 </h1>
                 <div className="object-cover ">
@@ -28,32 +37,46 @@ function Page(test) {
             </div>
             <div className="sm:w-1/2 pt-3 sm:px-3">
                 <div className="flex justify-between gap-3 sm:flex-col  lg:flex-row md:flex-wrap">
-                    <Button
-                        info={{
-                            link: `/carDetail/${car.Id}`,
-                            text: "Data",
-                        }}></Button>
-                    <Button
-                        info={{
-                            link: `/carDetail/${car.Id}/history`,
-                            text: "Historia",
-                        }}></Button>
-                    {/* <Button
-                        info={{
-                            link: `/cars`,
-                            text: "Våra Bilar",
-                        }}></Button> */}
+                    {/* Snygga gärna till med en komponent istället för div */}
+                    <div className="bg-main text-text-color text-center px-3 py-2 min-w-36 rounded-lg font-semibold border-text-color border hover:bg-text-color hover:text-main " onClick={() => (
+                        handleButtonClick("Data")
+                    )}>
+                        Data
+                    </div>
+                    <div className="bg-main text-text-color text-center px-3 py-2 min-w-36 rounded-lg font-semibold border-text-color border hover:bg-text-color hover:text-main " onClick={() => (
+                        handleButtonClick("test")
+                    )}>
+                        Historia
+                    </div>
                 </div>
-                <p>
-                    {car.Brand} {car.Year}
-                </p>
-                <p>Model: {car.Model}</p>
-                <p>
-                    Motor: {car.EngineInfo.Motor} {car.EngineInfo.Hp}{" "}
-                    {car.EngineInfo.Volym}
-                </p>
-                <p>Antal Tillverkade: {car.MiscInfo.NumbersManufactured}</p>
-                <p>Toppfart: {car.MiscInfo.TopSpeed}</p>
+                {selectedInfo === "Data" ? (
+                    <>
+                        <p>
+                            {car.Brand} {car.Year}
+                        </p>
+                        <p>Model: {car.Model}</p>
+                        <p>
+                            Motor: {car.EngineInfo.Motor} {car.EngineInfo.Hp}{" "}
+                            {car.EngineInfo.Volym}
+                        </p>
+                        <p>
+                            Antal Tillverkade:{" "}
+                            {car.MiscInfo.NumbersManufactured}
+                        </p>
+                        <p>Toppfart: {car.MiscInfo.TopSpeed}</p>
+                    </>
+                ) : (
+                    <>
+                        <p>
+                            {car.Brand} {car.Year}
+                        </p>
+                        <p>Model: {car.Model}</p>
+                        <p>Ägare: {car.MiscInfo.Owner}</p>
+                        <p>Inköpt: {car.MiscInfo.BoughtDate}</p>
+                        <p>Importerad: {car.MiscInfo.Imported}</p>
+                        <p>Info: {car.MiscInfo.Description}</p>
+                    </>
+                )}
             </div>
         </div>
     );
